9b25b99869b18576d852fbf97e06cce5aa5b7e48
SameerKhadtale <138295791+SameerKhadtale@users.noreply.github.com>
2023-09-24 15:55:29 +0530

    updates

--- frontend/src/components/Counter.jsx
+++ frontend/src/components/Counter.jsx
@@ -3,15 +3,29 @@ import { useState } from "react"
 const Counter = () => {
 
 
-const [counter, setCounter]= useState(0);
+
+const [counter, setCounter]= useState(false);
 console.log(counter,"counter")
+
+const [liked, setLiked]= useState(false);
+console.log(liked,"liked")
+
 // function Update(){
     // setCounter((prevValue) => prevValue+1)
+function multiTask(){
+    setLiked((flag)=>!flag)
+    setCounter((prevValue)=>prevValue+1)
 }
-return(
+return (
     <div>
        <h1>Counter - {counter}</h1>
-        <button onClick={()=>setCounter(1)}>Add Counter</button>
+        <button onClick={()=> setCounter((prevValue) => prevValue+1)}><b>+</b></button>
+        <button onClick={()=> setCounter((prevValue) => prevValue-1)}><b>-</b></button>
+        <button onClick={()=> setCounter((prevValue) => prevValue+10)}><b>+ 10 Counter</b></button>
+        <button onClick={()=> setCounter((prevValue) => prevValue-10)}><b>- 10 Counter</b></button>
+        <button onClick={()=> setLiked((value) => true)}><b>Like ?</b></button>
+        <button onClick={(multiTask)}><b>Like Counter</b></button>
     </div>
 )
+}
 export default Counter;
