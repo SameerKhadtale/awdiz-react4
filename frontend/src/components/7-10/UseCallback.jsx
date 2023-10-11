224c98121d25dd089aa2e25d91f2ac60661f1f03
SameerKhadtale <138295791+SameerKhadtale@users.noreply.github.com>
2023-10-11 14:53:22 +0530

    updatees

--- frontend/src/components/7-10/UseCallback.jsx
+++ frontend/src/components/7-10/UseCallback.jsx
@@ -3,11 +3,16 @@ import React, { useState } from 'react'
 const UseCallback = () => {
 const [counter,setCounter] = useState(0)
 function addCounter(){
-  
+      setCounter((value)=> value + 1)
 }
 
   return (
-    <div><h1>Todos</h1></div>
+    
+    <div>
+    <h1>Counter - {counter}</h1>
+{/* <Todos/> */}
+<button onClick={addCounter}>+</button>
+    </div>
   )
 }
 
