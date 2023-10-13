b176ad7c21799f65c83de8544cb89ecd997ccd46
SameerKhadtale <138295791+SameerKhadtale@users.noreply.github.com>
2023-10-13 17:03:29 +0530

    updates

--- frontend/src/components/16-09/StyledComponents.jsx
+++ frontend/src/components/16-09/StyledComponents.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import styled from 'styled-components'
 
 const StyledComponents = () => {
-    const Button = styled.button
+    const Button = styled.button`
     color : blue;
+    background-color : brown;
+    `
   return (
     <div>
-    <b>StyledComponents</b>
-    <Button>Styled Components</Button>
+    <Button style={{backgroundColor : "yellow"}}>Styled Components</Button>
     </div>
   )
 }
