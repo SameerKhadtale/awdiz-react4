06121df7b38ca6ad551776622af9ec8a2dcf95c0
SameerKhadtale <138295791+SameerKhadtale@users.noreply.github.com>
2023-10-18 14:18:57 +0530

    updates

--- frontend/src/components/20-09/DynamicStyles.jsx
+++ frontend/src/components/20-09/DynamicStyles.jsx
@@ -1,18 +1,21 @@
 import React, { useState } from 'react'
+import './DynamicStyles.css'
 
 const DynamicStyles = () => {
-  const [isButtonActive, setIsButtonActive]= useState(false);
+  const [isButtonActive, setIsButtonActive] = useState(true);
 
-  const handleButtonClick =()=>{
-  setIsButtonActive(!isButtonActive)
+  const handleButtonClick = () => {
+    setIsButtonActive(!isButtonActive)
   }
 
-  const buttonClassName = isButtonActive ? 'Active-Button' : 'Inactive-Button'
+  const buttonClassName = (isButtonActive ? 'Active-Button' : 'Inactive-Button');
   console.log(buttonClassName, "buttonClassName")
   return (
     <div>
       <h1>DynamicStyles</h1>
-      <button></button>
+      <button className={buttonClassName} onClick={handleButtonClick}>
+        {isButtonActive ? "Active" : "Inactive"}
+      </button>
     </div>
   )
 }
