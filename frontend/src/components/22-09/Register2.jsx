bb90a8781dfdafe6f144f8871de4bfd0e7256287
SameerKhadtale <138295791+SameerKhadtale@users.noreply.github.com>
2023-10-18 21:33:13 +0530

    update

--- frontend/src/components/22-09/Register2.jsx
+++ frontend/src/components/22-09/Register2.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Register2 = () => {
+
+  const[userData,setUserData]= useState({name :"",email : "",passowrd : ""})
+
+// console.log(userData,"userdata")
+  const handleChange = (event) =>{
+console.log(event.target.value)
+  }
   return (
     <div>
         <b>Register2</b>
         <form>
           <label>Name : </label><br></br>
-          <input type='name'></input><br></br>
+          <input type='text' onChange={handleChange}/><br/>
           <label>E-mail : </label><br></br>
-          <input type='e-mail'></input><br></br>
+          <input type='e-mail' onChange={handleChange}/><br/>
           <label>Password : </label><br></br>
-          <input type='password'></input><br></br>
+          <input type='password' onChange={handleChange}/><br/>
           <input type='submit'></input>
         </form>
     </div>
