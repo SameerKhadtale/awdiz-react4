f83825788241dde5f32a78472719699f2f9fdb1d
SameerKhadtale <138295791+SameerKhadtale@users.noreply.github.com>
2023-12-22 17:06:36 +0530

    updates

--- frontend/src/components/22-09/Register2.jsx
+++ frontend/src/components/22-09/Register2.jsx
@@ -3,16 +3,31 @@ import { useNavigate } from 'react-router-dom';
 import toast from 'react-hot-toast';
 import api from '../../helpers/AxiosConfig';
 
-
-
 const Register2 = () => {
-
-  const[userData,setUserData]= useState({name :"",email : "",passowrd : ""})
-
+const[userData,setUserData]= useState({name :"",email : "",passowrd : ""});
+const router = useNavigate();
 // console.log(userData,"userdata")
-  const handleChange = (event) =>{
-console.log(event.target.value)
+  
+const handleChange = (event) => {
+// console.log(event.target.value ,"value", event.taarget.name, "name")
+setUserData({...userData, [event.target.name]: event.target.value})
   }
+  
+  const sendDataToBackend = async(event)=>{
+event.preventDefault();
+alert("Data submitted to backend..")
+  if(userData.name && userData.email && userData.password) {
+if (userData.passowrd.length >=8) {
+try{
+
+}catch{
+  
+}
+}
+  }
+}
+  
+  
   return (
     <div>
         <b>Register</b>
