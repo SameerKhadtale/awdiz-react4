fe98956bfda3a81fc09e0dee0305f005bf1d0b97
SameerKhadtale <138295791+SameerKhadtale@users.noreply.github.com>
2024-02-02 19:20:15 +0530

    updates

--- frontend/src/components/1-10/SingleProductNew.jsx
+++ frontend/src/components/1-10/SingleProductNew.jsx
@@ -1,11 +1,32 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import api from '../helpers/AxiosConfig';
 import { useParams } from 'react-router-dom'
+import toast from 'react-hot-toast';
 import './SingleProductNew.css';
+import AuthContext from '../Context/AuthContext.jsx'
+
 
 const SingleProductNew = () => {
   const [productData, setProductData] = useState({});
   const { id } = useParams();
+  const {state} = useContext(AuthContext)
+
+  async function Cart(id) {
+    if (state.user.id && id) {
+        try {
+            const response = await api.post("/user/add-cart", { userId: state.user.id, productId: id })
+            if (response.data.success) {
+                toast.success(response.data.message)
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    } else {
+        toast.error("Please login to add product to cart.")
+    }
+}
+       
 
   useEffect(() => {
 
@@ -37,10 +58,12 @@ const SingleProductNew = () => {
             <h1>{productData.name}</h1>
             <h2>Price : {productData.price}$</h2>
             <h2>Category : {productData.category}</h2>
-            <h3>Description : {productData.description}</h3>
-            <h3>Rating : {productData.rating.rate}</h3>
-            <h3>Number of Rating : {productData.rating.count}</h3>
+            {/* <h3>Description : {productData.description}</h3> */}
+            {/* <h3>Rating : {productData.rating.rate}</h3> */}
+            {/* <h3>Number of Rating : {productData.rating.count}</h3> */}
             {/* <i class="fa-brands fa-instagram"></i> */}
+            <button onClick={() => Cart(productData._id)}>Cart</button>
+
 
           </div>
         </div>
--- frontend/src/components/15-09/SingleProduct.jsx
+++ frontend/src/components/15-09/SingleProduct.jsx
@@ -1,14 +1,15 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext,useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import api from '../helpers/AxiosConfig';
 import toast from 'react-hot-toast';
+import './Page.css';
+
 
 const SingleProduct = () => {
-
     const {id} =useParams();
-    console.log(id)
-    
-    const [productData, SetProductData] = useState({})
+    // console.log(id)
+    const {state} = useContext(AuthContext)  
+ const [productData, SetProductData] = useState({})
 
     async function getSingleProductData() {
         try {
@@ -16,6 +17,7 @@ const SingleProduct = () => {
             // const { data } = await axios.get(`http://localhost:8000/api/v1/product/single-product`)
             if (data.success) {
                 SetProductData(data.product)
+            
             }
         }
         catch (error) {
@@ -26,20 +28,25 @@ const SingleProduct = () => {
        
         if (id) {
             getSingleProductData()
+
         }
     }, [id])
+
+
 return (
     <div>
       {productData?._id ?
         <div id='parentDiv'>
           <div className='blackborder w-40' >
-            <img style={{ width: "60%", height: "85%" }} src={productData.image} /></div>
+            <img style={{ width:"100%" ,height:"100%", border:"5px solid black" }} src={productData.image} /></div>
           <div/>
-          <div className='blackborder w-40'>
+          <div style={{width :"40%", height : "300px", border :"5px solid black"}}>
             <h1>{productData.name}</h1>
             <h2>Price : {productData.price}$</h2>
             <h2>Category : {productData.category}</h2>
-            <h3>Description : {productData.description}</h3>
+            {/* <h3>Description : {productData.description}</h3> */}
+           <button onClick={() => Cart(productData._id)}>Cart</button>
+
         </div>
         </div>
         :
